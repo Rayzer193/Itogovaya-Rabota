9b61239dcb37e549afb865e3d61a1f1520e2dcad
Ivan Pavlov <rayzer_s@mail.ru>
2022-12-07 09:51:00 +0300

    final

--- programm/Program.cs
+++ programm/Program.cs
@@ -2,8 +2,9 @@
 //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 //лучше обойтись исключительно массивами.
+Console.Clear();
 
-/*System.Console.Write("Сколько слов вы будете вводить?(число от 1 до 100):");
+System.Console.Write("Сколько слов вы будете вводить?(число от 1 до 100):");
 int dlin = Convert.ToInt32(Console.ReadLine());
 
 string[] CreateArray(int Length = 100)
@@ -15,13 +16,7 @@ string[] CreateArray(int Length = 100)
         startArray[i] = Console.ReadLine();
     }
     return startArray;
-}*/
-
-Console.Clear();
-
-string[] Array1 = {"Hello", "2", "world", ":-)"};
-string[] Array2 = {"1234", "1567", "-2", "computer science"};
-string[] Array3 = {"Russia", "Denmark", "Kazan"};
+}
 
 string[] GetItogArray(string[] array)
 {
@@ -60,6 +55,4 @@ void printItog(string[] array)
 	Console.WriteLine();
 } 
 
-printItog(Array1);
-printItog(Array2);
-printItog(Array3);
+printItog(CreateArray());

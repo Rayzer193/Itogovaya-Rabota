a34fa0e5d971088c5d598fbcc34d19f79b3b4a0f
Ivan Pavlov <rayzer_s@mail.ru>
2022-12-07 08:58:40 +0300

    added itogovoi printrray

--- programm/Program.cs
+++ programm/Program.cs
@@ -2,18 +2,25 @@
 //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 //лучше обойтись исключительно массивами.
-System.Console.Write("Сколько строк вы будете вводить?(число от 1 до 100):");
+System.Console.Write("Сколько слов вы будете вводить?(число от 1 до 100):");
 int dlin = Convert.ToInt32(Console.ReadLine());
 
 void printArray(string[] array)
 {
-	if(array.Length == 0)
-	{
-		Console.WriteLine("Array is empty");
-	}
-	else
 	{
 		Array.ForEach(array, (str) => Console.Write($"{str} "));
 		Console.WriteLine();
 	}
+}
+
+string[] ItogArray= [];
+
+void printItog(string[] array)
+{
+	Console.WriteLine("Изначальный массив:");
+	printArray(array);
+	string[] shortArray = ItogArray;
+	Console.WriteLine("Итоговый массив (из слов содержащие 3 или меньше символов)");
+	printArray(shortArray);
+	Console.WriteLine();
 }
